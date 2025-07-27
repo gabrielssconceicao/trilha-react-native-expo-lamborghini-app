a966d203c508142e08192f1fec2ba1d59c22ad5d
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-07-27 18:40:06 -0300

    image import

---
+++ src/components/CardView/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button, Image } from 'react-native';
+import { styles } from './style';
+
+import Logo from '../../../assets/logo.png';
+
+export function CardView() {
+  return (
+    <View style={styles.imageContainer}>
+      <View style={styles.logoContainer}>
+        <Image source={Logo} style={styles.imageLogo} />
+      </View>
+    </View>
+  );
+}
---
+++ src/components/CardView/style.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: 200,
+    resizeMode: 'contain',
+  },
+  imageLogo: {
+    flex: 1,
+    width: '65%',
+    resizeMode: 'contain',
+  },
+});
